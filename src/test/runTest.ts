9fcd9a70bda142b9881ed044632df572f6a8f20b
kenherring <kennethherring@gmail.com>
2023-11-01 00:25:14 -0400

    cleanup

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,28 +4,13 @@ import { runTests } from '@vscode/test-electron';
 
 async function main() {
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-    // The path to the extension test runner script
-    // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
-      /**
-       * A list of launch arguments passed to VS Code executable, in addition to `--extensionDevelopmentPath`
-       * and `--extensionTestsPath` which are provided by `extensionDevelopmentPath` and `extensionTestsPath`
-       * options.
-       *
-       * If the first argument is a path to a file/folder/workspace, the launched VS Code instance
-       * will open it.
-       *
-       * See `code --help` for possible arguments.
-       */
       launchArgs: [
         'test_projects/proj1',
       ]
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -18,43 +18,27 @@ export function run(): Promise<void> {
 
   return new Promise((c, e) => {
     glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      console.log("glob-1")
       if (err) {
-        console.log("glob-2")
         return e(err);
       }
 
       // Add files to the test suite
-      console.log("index-1")
       files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-      console.log("index-2")
 
       try {
-        console.log("index-3")
         // Run the mocha test
         mocha.run(failures => {
-          console.log("index-4")
           if (failures > 0) {
-            console.log("index-5")
             console.error(`${failures} tests failed.`)
             e(new Error(`${failures} tests failed.`));
-            console.log("index-6")
           } else {
-            console.log("index-7")
             c();
-            console.log("index-8")
           }
-          console.log("index-9")
         });
-        console.log("index-10")
       } catch (err) {
-        console.log("index-11")
         e(err);
-        console.log("index-12")
       }
-      console.log("index-13")
       return
     });
-    console.log("index-14")
   });
 }
