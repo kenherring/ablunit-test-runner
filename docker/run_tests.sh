e46b4ca802d0ef30759743ef9a9b1273ce0fede1
kenherring <kennethherring@gmail.com>
2023-10-31 23:52:20 -0400

    add progress.cfg secret configuration

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -2,14 +2,20 @@
 set -eou pipefail
 
 GIT_BRANCH=$(git branch --show-current)
+PROGRESS_CFG_BASE64=$(base64 "$DLC/progress.cfg" | tr '\n' ' ')
+export GIT_BRANCH PROGRESS_CFG_BASE64
 
-docker run --rm kherring/ablunit-test-runner bash -c "set -eou pipefail;
+docker run --rm -it -e PROGRESS_CFG_BASE64 -e GIT_BRANCH \
+	kherring/ablunit-test-runner \
+	bash -c "set -eou pipefail; \
 	echo 'starting tests...'; \
 	git clone https://github.com/kenherring/ablunit-test-provider .; \
-	git checkout '"$GIT_BRANCH"'; \
+	git checkout \"\$GIT_BRANCH\"; \
 	npm install; \
 	npm run compile; \
-	sudo chmod +x /psc/dlc/bin/_progres; \
+	tr ' ' '\n' <<< \"\$PROGRESS_CFG_BASE64\" | base64 --decode > /psc/dlc/progress.cfg; \
 	sudo service dbus start; \
 	xvfb-run -a npm run test; \
-	echo 'done running tests'"
+	[ -f artifacts/mocha_results.xml ] || exit 1; \
+	echo 'done running tests';
+	"
