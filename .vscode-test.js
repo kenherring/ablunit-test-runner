8a2bd6dc0bfc2bde85fa28c09f05c3c1313f6cd2
kenherring <kennethherring@gmail.com>
2023-11-02 19:11:52 -0400

    Add tests - simple version to enable VSCode Test Runner

---
+++ .vscode-test.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vscode/test-cli');
+
+module.exports = defineConfig(
+	[{
+		label: 'unitTests',
+		files: 'out/test/**/*.test.js',
+		version: 'insiders',
+		workspaceFolder: './test_projects/proj1',
+		mocha: {
+			ui: 'tdd',
+			timeout: 20000
+		}
+	}
+]);
