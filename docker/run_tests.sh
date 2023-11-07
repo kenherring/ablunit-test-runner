6a543d4de19e2a5a46b2970ed1a694ca48851bca
kenherring <kennethherring@gmail.com>
2023-11-06 23:37:36 -0500

    fix circle results check; fix run_tests.sh in windows

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -5,13 +5,15 @@ GIT_BRANCH=$(git branch --show-current)
 PROGRESS_CFG_BASE64=$(base64 "$DLC/progress.cfg" | tr '\n' ' ')
 export GIT_BRANCH PROGRESS_CFG_BASE64
 
+PWD=$(pwd -W 2>/dev/null || pwd)
+
 ./cleanup.sh
 
 docker volume create --name vscode-test
 
 set -x
 docker run --rm -it -e PROGRESS_CFG_BASE64 -e GIT_BRANCH \
-	-v "$(pwd)":/home/circleci/ablunit-test-provider \
+	-v "$PWD":/home/circleci/ablunit-test-provider \
 	-v vscode-test:/home/circleci/project/.vscode-test \
 	kherring/ablunit-test-runner \
 	bash -c "/home/circleci/ablunit-test-provider/docker/entrypoint.sh;"
