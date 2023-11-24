6b05816d72d99223f1300db8780011b6db843a57
kenherring <kennethherring@gmail.com>
2023-11-23 21:08:03 -0500

    more refactoring for tests

--- src/test/indexCommon.ts
+++ src/test/indexCommon.ts
@@ -1,3 +1,4 @@
+import * as glob from "glob"
 import * as path from "path"
 import * as Mocha from "mocha"
 
@@ -41,12 +42,53 @@ export function setupMocha(projName: string) {
 		color: true,
 		ui: "tdd",
 		timeout: 20000,
-		reporter: 'mocha-multi-reporters',
+		reporter: 'mocha-junit-reporter',
 		reporterOptions: {
-			reporterEnabled: 'spec, mocha-junit-reporter',
-			mochaJunitReporterReporterOptions: {
-				mochaFile: 'artifacts/mocha_results_' + projName + '.xml'
-			}
+			mochaFile: 'artifacts/mocha_results_' + projName + '.xml'
 		}
+		// reporter: 'mocha-multi-reporters',
+		// reporterOptions: {
+		// 	reporterEnabled: 'spec, mocha-junit-reporter',
+		// 	mochaJunitReporterReporterOptions: {
+		// 		mochaFile: 'artifacts/mocha_results_' + projName + '.xml'
+		// 	}
+		// }
 	})
+}
+
+export function runTests (projName: string, mocha: Mocha, nyc: any) {
+	const testsRoot = path.resolve(__dirname, "..");
+	return new Promise<void>((c, e) => {
+		glob("**/**." + projName + ".test.js", {
+			cwd: testsRoot
+		}, (err, files) => {
+			if (err) {
+				return e(err);
+			}
+
+			// Add files to the test suite
+			files.forEach((f) => {
+				mocha.addFile(path.resolve(testsRoot, f))
+			});
+
+			try {
+				// Run the mocha test
+				mocha.run(async (failures) => {
+					if (nyc) {
+						nyc.writeCoverageFile();
+						await nyc.report();
+					}
+
+					if (failures > 0) {
+						console.log(`${failures} tests failed.`)
+						e(new Error(`${failures} tests failed.`))
+					}
+					c();
+				});
+			} catch (err) {
+				console.error('[index_' + projName + '.ts] catch err= ' + err);
+				e(err);
+			}
+		});
+	});
 }
--- src/test/suite/index_proj0.ts
+++ src/test/suite/index_proj0.ts
@@ -1,45 +1,9 @@
-import * as glob from "glob"
-import * as path from "path"
-import { setupMocha, setupNyc } from "../indexCommon"
+import { runTests, setupMocha, setupNyc } from "../indexCommon"
 
 const projName = 'proj0'
 
 export function run(): Promise <void> {
 	const nyc = setupNyc(projName)
 	const mocha = setupMocha(projName)
-
-	const testsRoot = path.resolve(__dirname, "..");
-	return new Promise((c, e) => {
-		glob("**/**." + projName + ".test.js", {
-			cwd: testsRoot
-		}, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach((f) => {
-				mocha.addFile(path.resolve(testsRoot, f))
-			});
-
-			try {
-				// Run the mocha test
-				mocha.run(async (failures) => {
-					if (nyc) {
-						nyc.writeCoverageFile();
-						await nyc.report();
-					}
-
-					if (failures > 0) {
-						console.log(`${failures} tests failed.`)
-						e(new Error(`${failures} tests failed.`))
-					}
-					c();
-				});
-			} catch (err) {
-				console.error('[index_' + projName + '.ts] catch err= ' + err);
-				e(err);
-			}
-		});
-	});
+	return runTests(projName, mocha, nyc)
 }
