b9fb9240bf20aaa9896041779fa9f47677d01a33
kenherring <kennethherring@gmail.com>
2023-11-25 12:43:12 -0500

    uncomment tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,11 +3,11 @@ import { runTests } from '@vscode/test-electron';
 
 
 async function main() {
-	// await testProject("proj0")
-	// await testProject("proj1")
-	// await testProject("proj2")
+	await testProject("proj0")
+	await testProject("proj1")
+	await testProject("proj2")
 	await testProject("proj3", "proj3_debugLines")
-	// await testProject("proj4")
+	await testProject("proj4")
 }
 
 async function testProject(projName: string, projDir?: string) {
