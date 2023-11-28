7ae1c41009f459b2c60d685b2c7e17b998e25b61
kenherring <kennethherring@gmail.com>
2023-11-28 00:17:15 -0500

    add test to docker run

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -8,6 +8,7 @@ async function main() {
 	await testProject("proj3", "proj3_debugLines")
 	await testProject("proj4")
 	await testProject("proj5", "proj5_suites")
+	await testProject("proj7", "proj7_load_performance")
 }
 
 async function testProject(projName: string, projDir?: string) {
