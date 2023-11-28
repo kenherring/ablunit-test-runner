8646e6e6885fc3e3ece92e34b217f9fb5f5b3e5e
kenherring <kennethherring@gmail.com>
2023-11-27 21:16:12 -0500

    re-enable all tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,11 +4,11 @@ import { runTests } from '@vscode/test-electron'
 
 
 async function main() {
-	// await testProject("proj0")
-	// await testProject("proj1")
-	// await testProject("proj2")
-	// await testProject("proj3", "proj3_debugLines")
-	// await testProject("proj4")
+	await testProject("proj0")
+	await testProject("proj1")
+	await testProject("proj2")
+	await testProject("proj3", "proj3_debugLines")
+	await testProject("proj4")
 	await testProject("proj5", "proj5_suites")
 }
 
