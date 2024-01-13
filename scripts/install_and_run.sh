ae7fdc2f1bd3e138f186852626f1d9ab4da15fb8
kenherring <kennethherring@gmail.com>
2024-01-13 11:41:28 -0500

    install_and_run.sh

--- scripts/install_and_run.sh
+++ scripts/install_and_run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 set -eou pipefail
 
-rm -f ./*.vsix
+initialize() {
+    rm -f ./*.vsix
+    if [ -z "${CIRCLE_BRANCH:-}" ]; then
+        CIRCLE_BRANCH="$(git branch --show-current)"
+    fi
+}
 
-npm install
-vsce package --pre-release --githubBranch "$(git branch --show-current)"
-cd dummy-ext
-npm run compile
-npm run test:install-and-run
+build_extension() {
+    npm install
+    vsce package --pre-release --githubBranch "$CIRCLE_BRANCH"
+}
+
+compile_install_run() {
+    cd dummy-ext
+    npm run compile
+    npm run test:install-and-run
+}
+
+########## MAIN BLOCK ##########
+initialize
+build_extension
+compile_install_run
 echo "install_and_run.sh completed successfully!"
