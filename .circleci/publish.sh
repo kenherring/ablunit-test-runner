e12b554587bcfe54d0b3459a1334afe03900e7e9
kenherring <kennethherring@gmail.com>
2024-01-21 17:39:56 -0500

    publish

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -8,16 +8,16 @@ main_block () {
         CIRCLE_BRANCH=$(git branch --show-current)
     fi
     ## CIRCLE_TAG=$(git describe --tags --abbrev=0)
-    
+
     if [ -z "$CIRCLE_TAG" ]; then
         echo "exit: CIRCLE_TAG is empty, cannot publish release..."
         exit 1
     fi
-    if [ ! -f ablunit-test-runner-${CIRCLE_TAG#v}.vsix ]; then
+    if [ ! -f "ablunit-test-runner-${CIRCLE_TAG#v}.vsix" ]; then
         echo "ERROR: ablunit-test-runner-${CIRCLE_TAG#v}.vsix not found, creating it now..."
         exit 1
     fi
-    vsce publish --pre-release --githubBranch "main" --packagePath ablunit-test-runner-v${CIRCLE_TAG}.vsix
+    vsce publish --pre-release --githubBranch "main" --packagePath "ablunit-test-runner-v${CIRCLE_TAG}.vsix"
 }
 
 attach_to_github_release () {
