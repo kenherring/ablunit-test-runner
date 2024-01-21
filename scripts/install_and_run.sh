f1dfe94db575be139982818df28475b92a998bdb
kenherring <kennethherring@gmail.com>
2024-01-21 16:28:14 -0500

    install and run

--- scripts/install_and_run.sh
+++ scripts/install_and_run.sh
@@ -2,8 +2,12 @@
 set -euo pipefail
 
 initialize() {
-    echo "[$0 ${FUNCNAME[0]}]"
+    echo "[$0 ${FUNCNAME[0]}] pwd=$(pwd)"
+    cd /home/circleci/project
+    echo "[$0 ${FUNCNAME[0]}] pwd=$(pwd)"
+
     CIRCLECI=${CIRCLECI:-false}
+    PACKAGE_VERSION=$(node -p "require('./package.json').version")
 
     rm -f ./*.vsix
     if [ -z "${CIRCLE_BRANCH:-}" ]; then
@@ -14,6 +18,21 @@ initialize() {
     if ! $CIRCLECI; then
         npm install
         vsce package --pre-release --githubBranch "$CIRCLE_BRANCH"
+    fi
+
+    local VSIX_COUNT
+    VSIX_COUNT=$(find . -name 'ablunit-test-runner-*.vsix' | wc -l)
+    if [ "$VSIX_COUNT" != "1" ]; then
+        echo "ERROR: expected 1 vsix file, found $VSIX_COUNT" >&2
+        if [ "$VSIX_COUNT" != "0" ]; then
+            echo "*.vsix files found:" >&2
+            find . -name 'ablunit-test-runner-*.vsix' >&2
+        fi
+        exit 1
+    fi
+    if [ ! -f "./ablunit-test-runner-$PACKAGE_VERSION.vsix" ]; then
+        echo "ERROR: expected vsix file to be named 'ablunit-test-runner-$PACKAGE_VERSION.vsix'" >&2
+        exit 1
     fi
 }
 
