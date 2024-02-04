5d7f7cc2d46e4a770cc958a6aabcdb25685d1199
kenherring <kennethherring@gmail.com>
2024-02-04 14:47:27 -0500

    incremental

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('@vscode/test-cli')
-const fs = require('fs')
-const { createTestConfig } = require('./out/test/createTestConfig')
-
-// Generate .vscode-test.config.json by running this script:
-//   node ./out/test/createTestConfig.js
+// // Generate .vscode-test.config.json by running this script:
+// //   node ./out/test/createTestConfig.js
+// const fs = require('fs')
 // const testConfig = JSON.parse(fs.readFileSync('./.vscode-test.config.json', 'utf8'))
 
+const { createTestConfig } = require('./out/test/createTestConfig')
 const testConfig = createTestConfig()
+
+const { defineConfig } = require('@vscode/test-cli')
 module.exports = defineConfig(testConfig)
