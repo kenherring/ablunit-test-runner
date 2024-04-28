0fed4c7d736eee8459290a4d0ee38b3e50c7c45d
kenherring <kennethherring@gmail.com>
2024-04-28 18:13:43 -0400

    package.sh

--- .circleci/package.sh
+++ .circleci/package.sh
@@ -30,7 +30,7 @@ initialize () {
 
 package () {
     echo "[$(date +%Y-%m-%d:%H:%M:%S) $0 ${FUNCNAME[0]}]"
-    rm -f *.vsix
+    rm -f ./*.vsix
     package_version stable
     # package_version insiders
 }
