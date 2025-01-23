f1473fed85d6af5fa299bfad334a7e02f9787c1b
kenherring <kennethherring@gmail.com>
2025-01-23 16:24:22 -0500

    bump tag updates

--- .circleci/bump_version.sh
+++ .circleci/bump_version.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 
-## when we want to create a new non-prerelease simply run `npm version patch`
-## or `npm version <tag>` and push the changes.  this should give us an even
-## numbered patch that will flow to the main branch
-
 . scripts/common.sh
 
 main () {
+    log_it
+    PACKAGE_VERSION=$(jq -r '.version' package.json)
     if [ "${CIRCLE_BRANCH:-}" = "main" ]; then
-        log_it "nothing to update for main branch"
+        push_tag
         return
     fi
 
-    PACKAGE_VERSION=$(jq -r '.version' package.json)
-    GIT_TAG_VERSION=$(git tag -l --sort=version:refname | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | grep -v '\.999$' | tail -1)
-    echo "PACKAGE_VERSION=$PACKAGE_VERSION"
-    echo "GIT_TAG_VERSION=$GIT_TAG_VERSION"
-
-    if [ "$GIT_TAG_VERSION" = "$PACKAGE_VERSION" ]; then
-
-        PATCH_NUM=$((${PACKAGE_VERSION##*.} + 1))
-        echo "PATCH_NUM=$PATCH_NUM"
-        if [ "$((PATCH_NUM % 2))" == "0" ]; then
-            PATCH_NUM=$((PATCH_NUM+1))
-            echo "PATCH_NUM=$PATCH_NUM"
-        fi
-        BUMP_TO_VERSION=${PACKAGE_VERSION%.*}.$((PATCH_NUM))
-        echo "BUMP_TO_VERSION=$BUMP_TO_VERSION"
-
-        npm version "$BUMP_TO_VERSION"
-        BUMP_TO_VERSION=$(jq -r '.version' package.json)
-        log_it "bumped version to '$BUMP_TO_VERSION'"
-        echo "$BUMP_TO_VERSION"
-        git push
+    GIT_TAG_MATCH=$(git tag -l --sort=version:refname | grep -E "^$PACKAGE_VERSION$")
+    if [ -z "$GIT_TAG_MATCH" ]; then
+        log_it "no tag exists for PACKAGE_VERSION=$PACKAGE_VERSION, nothing more to do..."
+        return 0
     fi
+
+    bump_prerelease_version
+}
+
+bump_prelease_version () {
+    log_it "PACKAGE_VERSION=$PACKAGE_VERSION"
+    PATCH_NUM=$((${PACKAGE_VERSION##*.} + 1))
+    if [ "$((PATCH_NUM % 2))" = "0" ]; then
+        ## always bump to an odd number for pre-release
+        ## even number releases are set manually and pushed
+        PATCH_NUM=$((PATCH_NUM + 1))
+    fi
+
+    BUMP_TO_VERSION=${PACKAGE_VERSION%.*}.$PATCH_NUM
+    while git tag -l --sort=version:refname | grep -q -E "^$BUMP_TO_VERSION$"; do
+        log_it "tag '$BUMP_TO_VERSION' already exists..."
+        PATCH_NUM=$((PATCH_NUM + 2))
+        BUMP_TO_VERSION=${PACKAGE_VERSION%.*}.$PATCH_NUM
+    done
+    log_it "bumping version to $BUMP_TO_VERSION"
+
+    npm version "$BUMP_TO_VERSION" --no-tag-git-version -m "Bump version to prerelease %s"
+    git push tag origin "$BUMP_TO_VERSION"
+}
+
+## when building the main branch, set the tag to the version in package.json
+push_tag () {
+    log_it "PACKAGE_VERSION=$PACKAGE_VERSION"
+    PATCH_NUM=$((${PACKAGE_VERSION##*.} + 1))
+
+    log_it "create tag $PACKAGE_VERSION and push"
+    npm version "$PACKAGE_VERSION" --allow-same-ver
+    git push tag origin "PACKAGE_VERSION"
 }
 
 ########## MAIN BLOCK ##########
