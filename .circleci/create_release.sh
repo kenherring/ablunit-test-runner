3691363016403f8a0f8284bf3051ddd9cc48a3d5
Ken Herring <kennethherring@gmail.com>
2025-01-24 23:19:59 -0500

    Profile parsing of performance improvement for multiple data files (#235)

---
+++ .circleci/create_release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+## Automatically publish pre-release branches
+## Creates a release in GitHub with the current version as a tag and
+## This tag will run another workflow to publish the release to the marketplace
+
+. scripts/common.sh
+
+main () {
+    log_it
+
+    PACKAGE_VERSION=$(jq -r '.version' package.json)
+    if git tag -l --sort=version:refname | grep -q "^$PACKAGE_VERSION$"; then
+        log_error "tag exists for PACKAGE_VERSION=$PACKAGE_VERSION"
+        exit 1
+    fi
+
+    PRERELEASE=false
+    PATCH_VERSION=${PACKAGE_VERSION##*.}
+    if [ "$((PATCH_VERSION % 2))" = "1" ]; then
+        PRERELEASE=true
+    fi
+
+    LATEST_RELEASE_TAG=$(git tag -l '[0-9].*' --sort=version:refname | grep -E "^[0-9]+\.[0-9]+\.[0-9]*[0,2,4,6,8]$" | tail -1)
+
+    ARGS=()
+    if $PRERELEASE; then
+        ARGS+=(--prerelease)
+        ARGS+=(--title "$PACKAGE_VERSION (prerelease)")
+        if ! ${CIRCLECI:-false}; then
+            ARGS+=(--draft)
+        fi
+    else
+        ARGS+=(--draft)
+        ARGS+=(--title "$PACKAGE_VERSION")
+    fi
+    ARGS+=(--generate-notes)
+    ARGS+=(--notes-start-tag "$LATEST_RELEASE_TAG")
+    ARGS+=(--target $(git rev-parse HEAD))
+
+    curl -L https://github.com/cli/cli/releases/download/v2.65.0/gh_2.65.0_linux_amd64.deb -o /tmp/gh_2.65.0_linux_amd64.deb
+    sudo dpkg -i /tmp/gh_2.65.0_linux_amd64.deb
+
+    gh release create "$PACKAGE_VERSION" "${ARGS[@]}"
+    log_it "release created for PACKAGE_VERSION=$PACKAGE_VERSION"
+}
+
+########## MAIN BLOCK ##########
+main
