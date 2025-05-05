e986624258988380558e1ae94415aff1c9627f15
kenherring <kennethherring@gmail.com>
2025-05-05 02:00:59 -0400

    proj7B.2

--- test/suites/proj7B.test.ts
+++ test/suites/proj7B.test.ts
@@ -80,6 +80,7 @@ suite('proj7B - Extension Test Suite', () => {
 		runAllTests().catch((e: unknown) => { log.info('runAllTests got error: ' + e) })
 		await sleep(100)
 			.then(() => { return waitForTestRunStatus(RunStatus.Constructed) })
+			.then(() => { return sleep(100) })
 
 		const elapsedCancelTime = await cancelTestRun(false)
 		assert.durationLessThan(elapsedCancelTime, maxCancelTime)
