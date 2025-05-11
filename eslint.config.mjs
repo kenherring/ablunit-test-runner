be5f6d82db5a1d960a5d2005ed50501bc255ec1d
kenherring <kennethherring@gmail.com>
2025-05-11 01:25:47 -0400

    update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,146 +1,142 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
-import stylistic from '@stylistic/eslint-plugin-ts'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import promise from 'eslint-plugin-promise'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+)
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-})
+// import { defineConfig } from 'eslint/config'
+// import js from '@eslint/js'
+// import ts from 'typescript-eslint'
+// import promise from 'eslint-plugin-promise'
+// import tsParser from '@typescript-eslint/parser'
+// import path from 'node:path'
+// import { fileURLToPath } from 'node:url'
 
-export default [
-	{
-		ignores: [
-			'**/dummy-ext/',
-			'**/test_projects/',
-			'**/esbuild.js',
-		],
-	},
-	...compat.extends(
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended-type-checked',
-		'plugin:@typescript-eslint/strict-type-checked',
-		'plugin:@typescript-eslint/stylistic-type-checked',
-		'plugin:promise/recommended',
-	),
-	{
-		plugins: {
-			'@stylistic': stylistic,
-			'@typescript-eslint': typescriptEslint,
-			promise,
-		},
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
-		languageOptions: {
-			globals: {
-				Atomics: 'readonly',
-				SharedArrayBuffer: 'readonly',
-			},
 
-			parser: tsParser,
-			ecmaVersion: 5,
-			sourceType: 'script',
+// export default defineConfig([
+// 	{
+// 		ignores: [
+// 			'**/dummy-ext/',
+// 			'**/test_projects/',
+// 			'**/esbuild.js',
+// 			'.vscode-test/**',
+// 			'.worktrees/**',
+// 			'dist/**',
+// 			'resources/ADE/**',
+// 		],
+// 	},
+// 	js.configs.recommended,
+// 	ts.configs.strictTypeChecked,
+// 	ts.configs.stylisticTypeChecked,
+// 	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+// 	promise.configs.
 
-			parserOptions: {
-				project: './tsconfig.json',
-				tsconfigRootDir: '.',
-			},
-		},
+// 	{
+// 		languageOptions: {
+// 			globals: {
+// 				Atomics: 'readonly',
+// 				SharedArrayBuffer: 'readonly',
+// 			},
 
-		rules: {
-			'@stylistic/indent': ['error', 'tab'],
+// 			parser: tsParser,
+// 			ecmaVersion: 5,
+// 			sourceType: 'script',
 
-			'@stylistic/comma-spacing': ['warn', {
-				before: false,
-				after: true,
-			}],
+// 			parserOptions: {
+// 				project: './tsconfig.json',
+// 				tsconfigRootDir: '.',
+// 			},
+// 		},
 
-			'@stylistic/no-extra-parens': 'warn',
+// 		rules: {
+// 			// '@stylistic/indent': ['error', 'tab'],
 
-			'@typescript-eslint/no-restricted-types': ['error', {
-				types: {
-					Object: 'Use {} instead.',
-					String: 'Use "string" instead.',
-					Number: 'Use "number" instead.',
-					Boolean: 'Use "boolean" instead.',
-				},
-			}],
+// 			// '@stylistic/comma-spacing': ['warn', {
+// 			// 	before: false,
+// 			// 	after: true,
+// 			// }],
 
-			'@typescript-eslint/naming-convention': ['error', {
-				selector: 'interface',
-				format: ['PascalCase'],
+// 			// '@stylistic/no-extra-parens': 'warn',
 
-				custom: {
-					regex: '^I[A-Z]',
-					match: true,
-				},
-			}],
+// 			'@typescript-eslint/no-restricted-types': ['error', {
+// 				types: {
+// 					Object: 'Use {} instead.',
+// 					String: 'Use "string" instead.',
+// 					Number: 'Use "number" instead.',
+// 					Boolean: 'Use "boolean" instead.',
+// 				},
+// 			}],
 
-			'@typescript-eslint/no-confusing-non-null-assertion': 'warn',
+// 			'@typescript-eslint/naming-convention': ['error', {
+// 				selector: 'interface',
+// 				format: ['PascalCase'],
 
-			'@typescript-eslint/no-floating-promises': ['error', {
-				checkThenables: true,
-			}],
+// 				custom: {
+// 					regex: '^I[A-Z]',
+// 					match: true,
+// 				},
+// 			}],
 
-			'@typescript-eslint/no-misused-promises': 'error',
-			'@typescript-eslint/no-non-null-assertion': 0,
-			'@typescript-eslint/no-unnecessary-condition': 0,
-			'no-unused-vars': 'off',
+// 			'@typescript-eslint/no-confusing-non-null-assertion': 'warn',
 
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					'args': 'all',
-					'argsIgnorePattern': '^_',
-					'caughtErrors': 'all',
-					'caughtErrorsIgnorePattern': '^_',
-					'destructuredArrayIgnorePattern': '^_',
-					'varsIgnorePattern': '^_',
-					'ignoreRestSiblings': true,
-				}
-			],
+// 			'@typescript-eslint/no-floating-promises': ['error', {
+// 				checkThenables: true,
+// 			}],
 
-			'@typescript-eslint/prefer-readonly': 'warn',
-			'@typescript-eslint/restrict-plus-operands': 'off',
-			'@typescript-eslint/return-await': ['error', 'always'],
-			'@typescript-eslint/switch-exhaustiveness-check': ['warn', {
-				considerDefaultExhaustiveForUnions: true,
-			}],
-			'promise/catch-or-return': ['warn', {
-				allowThen: true,
-			}],
-			'promise/no-callback-in-promise': 'off',
+// 			'@typescript-eslint/no-misused-promises': 'error',
+// 			'@typescript-eslint/no-non-null-assertion': 0,
+// 			'@typescript-eslint/no-unnecessary-condition': 0,
+// 			'no-unused-vars': 'off',
 
-			'promise/always-return': ['warn', {
-				ignoreLastCallback: true
-			}],
+// 			'@typescript-eslint/no-unused-vars': [
+// 				'error',
+// 				{
+// 					'args': 'all',
+// 					'argsIgnorePattern': '^_',
+// 					'caughtErrors': 'all',
+// 					'caughtErrorsIgnorePattern': '^_',
+// 					'destructuredArrayIgnorePattern': '^_',
+// 					'varsIgnorePattern': '^_',
+// 					'ignoreRestSiblings': true,
+// 				}
+// 			],
 
-			'no-console': 'warn',
-			'no-empty': 'warn',
-			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+// 			'@typescript-eslint/prefer-readonly': 'warn',
+// 			'@typescript-eslint/restrict-plus-operands': 'off',
+// 			'@typescript-eslint/return-await': ['error', 'always'],
+// 			'@typescript-eslint/switch-exhaustiveness-check': ['warn', {
+// 				considerDefaultExhaustiveForUnions: true,
+// 			}],
+// 			'promise/catch-or-return': ['warn', {
+// 				allowThen: true,
+// 			}],
+// 			'promise/no-callback-in-promise': 'off',
 
-			'no-trailing-spaces': ['error', {
-				skipBlankLines: false,
-			}],
+// 			'promise/always-return': ['warn', {
+// 				ignoreLastCallback: true
+// 			}],
 
-			'prefer-promise-reject-errors': 'error',
-			quotes: ['warn', 'single'],
-			semi: ['error', 'never'],
-			'require-await': 'error',
-			'space-before-blocks': ['error', 'always'],
-			'space-before-function-paren': ['warn', 'always'],
-			'space-in-parens': ['warn', 'never'],
+// 			'no-console': 'warn',
+// 			'no-empty': 'warn',
+// 			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
 
-			'spaced-comment': ['error', 'always', {
-				markers: ['/'],
-			}],
-		},
-	}]
+// 			'prefer-promise-reject-errors': 'error',
+// 			quotes: ['warn', 'single'],
+// 			semi: ['error', 'never'],
+// 			'require-await': 'error',
+// 			'space-before-blocks': ['error', 'always'],
+// 			'space-before-function-paren': ['warn', 'always'],
+// 			'space-in-parens': ['warn', 'never'],
+
+// 			'spaced-comment': ['error', 'always', {
+// 				markers: ['/'],
+// 			}],
+// 		},
+// 	}
+// ])
