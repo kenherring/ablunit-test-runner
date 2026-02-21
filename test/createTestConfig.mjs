cbd46d405b21e260950a03bc7caacaf389f8d510
kenherring <kennethherring@gmail.com>
2026-02-20 23:31:56 -0500

    skip test SourceMapRCodeParser project

--- test/createTestConfig.mjs
+++ test/createTestConfig.mjs
@@ -286,7 +286,7 @@ function getTests () {
 
 	const skipProjects = [
 		// 'OpenedgeProjectParser',
-		// 'SourceMapRCodeParser',
+		'SourceMapRCodeParser',
 		// 'SourceMapXrefParser',
 		// 'TestProfileParser',
 		// 'AtStart',
