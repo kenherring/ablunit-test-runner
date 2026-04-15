b46fe0a9909b3426d7fde4610e3ff774a3a13f54
kenherring <kennethherring@gmail.com>
2026-04-15 05:39:49 -0400

    Various fixes for development tooling

--- scripts/common.sh
+++ scripts/common.sh
@@ -82,12 +82,12 @@ validate_version_updated() {
 	log_it 'validating version matches throughout the project...' >&2
 	local PACKAGE_VERSION SONAR_PROJECT_VERSION CHANGELOG_VERSION
 	PACKAGE_VERSION=$(jq -r '.version' package.json)
-	SONAR_PROJECT_VERSION=$(grep -E '^sonar.projectVersion=' sonar-project.properties | cut -d'=' -f2)
+	# SONAR_PROJECT_VERSION=$(grep -E '^sonar.projectVersion=' sonar-project.properties | cut -d'=' -f2)
 
-	if [ "$PACKAGE_VERSION" != "$SONAR_PROJECT_VERSION" ]; then
-		log_error "package.json version ($PACKAGE_VERSION) does not match 'sonar.projectVersion' ($SONAR_PROJECT_VERSION) in 'sonar-project.properties'"
-		exit 1
-	fi
+	# if [ "$PACKAGE_VERSION" != "$SONAR_PROJECT_VERSION" ]; then
+	# 	log_error "package.json version ($PACKAGE_VERSION) does not match 'sonar.projectVersion' ($SONAR_PROJECT_VERSION) in 'sonar-project.properties'"
+	# 	exit 1
+	# fi
 
 	CHANGELOG_VERSION=$(head CHANGELOG.md -n 1 | cut -d'[' -f2 | cut -d']' -f1)
 	if [ "$PACKAGE_VERSION" != "$CHANGELOG_VERSION" ]; then
